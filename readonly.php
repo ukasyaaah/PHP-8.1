355527b0f380de6674cef12613cc308fd72afd54
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-06 13:49:43 +0700

    readonly

---
+++ readonly.php
@@ -0,0 +1,17 @@
+<?php
+
+class  Film
+{
+    public function __construct(
+        public readonly string $judul,
+        public readonly string  $genre,
+    ) {}
+}
+
+
+$film = new Film(
+    judul: 'Bring Her Back',
+    genre: 'Horror'
+);
+
+var_dump($film->judul);
